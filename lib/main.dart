dab0eeb16758c09ec5f6fc1e559f2f9b170be3cd
Aakib <deepak.bajwal@infostride.com>
2021-03-11 02:00:55 +0530

    nlothing

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'AAKIB',
       theme: ThemeData(
-        splashColor: Colors.blue,
+        // splashColor: Colors.blue,
         primarySwatch: Colors.green,
       ),
       home: MainPage(),
